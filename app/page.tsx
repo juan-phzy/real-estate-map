7c0895b1ba7d178e3625f4966d628c6f5ac3b0d6
juan-phzy <jp.hernandez.yz@gmail.com>
2024-03-02 17:16:37 -0500

    Basic Project Structure Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const page = () => {
-	return <div className="main">Home Page</div>;
+	return (
+		<section className="main">
+			<div className="text-4xl">This is the home page</div>
+			<div className="bg-slate-600 w-[50%] h-fit p-9 flex justify-center items-center">
+				And this is a content block
+			</div>
+		</section>
+	);
 };
 
 export default page;
