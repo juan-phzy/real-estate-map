6ce972f51f623d5142afb1963daec0c952bbd211
juan-phzy <jp.hernandez.yz@gmail.com>
2024-03-08 03:20:32 -0500

    Added sample material ui component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import BasicTabs from "@/components/shared/BasicTabs";
 
 const page = () => {
 	return (
 		<section className="main">
 			<div className="text-4xl">This is the home page</div>
-			<div className="bg-slate-600 w-[50%] h-fit p-9 flex justify-center items-center">
-				And this is a content block
+			<div className="bg-slate-200 text-black flex justify-center items-center gap-10 w-[70%] h-fit p-9 flex justify-center items-center">
+				And this is a content block with a material ui sample component
+				<BasicTabs />
 			</div>
 		</section>
 	);
