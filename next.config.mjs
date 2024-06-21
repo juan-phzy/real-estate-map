cd10bc2242d37581ba1965638b97177f0df3444c
Juan Hernandez <jp.hernandez.yz@gmail.com>
2024-06-20 21:53:06 -0400

    env

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,13 @@ const nextConfig = {
 		// your project has type errors.
 		// !! WARN !!
 		ignoreBuildErrors: true,
-	}
+	},
+	env: {
+		// Reference a variable that was defined in the .env.local file and make it available at Build Time
+		MAPBOX_TOKEN:"pk.eyJ1Ijoic3ZheXNlciIsImEiOiJjbGgwbzl5NXcwdmMzM2VwdTkya2J6cDVmIn0.VrQewCt9w1K8QPsLzuDZjg",
+		GOOGLE_API_KEY:"AIzaSyAYtYh4QOSbcTR3v6Qy8VBiOW9qLLoZMU0",
+
+	},
 };
 
 export default nextConfig;
